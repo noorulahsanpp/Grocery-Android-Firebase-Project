26f171d015dc8a35ea8ae99e014c9ac5f6c5b703
Pinky <pinkyabras101@gmail.com>
2020-08-24 13:53:58 +0530

    login design

---
+++ app/src/main/java/com/example/grocery/Login.java
@@ -0,0 +1,15 @@
+package com.example.grocery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+    }
+}
